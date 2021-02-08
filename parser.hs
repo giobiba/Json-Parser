7ab9245501fecfa99d4467a96f66671f2836f155
giobiba <g.blanar82@gmail.com>
2021-02-08 12:13:45 +0200

    finished parser

--- parser.hs
+++ parser.hs
@@ -111,7 +111,23 @@ jsonWhitespaceGen =
     . elements
     $ [' ' , '\n' , '\r' , '\t']
 
--- parser
+stringify :: JValue -> Gen String
+stringify = pad . go
+  where
+    surround l r j = l ++ j ++ r
+    pad gen = surround <$> jsonWhitespaceGen <*> jsonWhitespaceGen <*> gen
+    commaSeparated = pad . pure . intercalate ","
+
+    go value = case value of
+      JArray elements ->
+        mapM (pad . stringify) elements
+          >>= fmap (surround "[" "]") . commaSeparated
+      JObject kvs ->
+        mapM stringifyKV kvs >>= fmap (surround "{" "}") . commaSeparated
+      _           -> return $ show value
+
+    stringifyKV (k, v) =
+      surround <$> pad (pure $ showJSONString k) <*> stringify v <*> pure ":"
 
 newtype Parser i o = Parser  {runParser :: i -> Maybe (i, o)}
 
@@ -135,8 +151,8 @@ instance Monad (Parser i) where
         Just (rest, o) -> runParser (f o) rest
 
 -- first version
--- charParse :: Char -> Parser String Char
--- charParse c = Parse $ \case
+-- char :: Char -> Parser String Char
+-- char c = Parse $ \case
 --    (x:xs) | x == c -> Just (xs, x)
 --    _               -> Nothing
 
@@ -146,8 +162,8 @@ satisfy predicate = Parser $ \case
     (x:xs) | predicate x  -> Just (xs, x)
     _                     -> Nothing
 
-charParse :: Char -> Parser String Char
-charParse c  = satisfy (== c)
+char :: Char -> Parser String Char
+char c  = satisfy (== c)
 
 digitParse :: Parser String Int
 digitParse = digitToInt <$> satisfy isDigit
@@ -156,17 +172,17 @@ digitParse = digitToInt <$> satisfy isDigit
 
 stringParse :: String -> Parser String String
 stringParse ""     = pure ""
-stringParse (c:cs) = (:) <$> charParse c <*> stringParse cs
+stringParse (c:cs) = (:) <$> char c <*> stringParse cs
 
-jNullParse :: Parser String JValue
-jNullParse = stringParse "null" $> JNull
+jNull :: Parser String JValue
+jNull = stringParse "null" $> JNull
 
-jBoolParse :: Parser String JValue
-jBoolParse = stringParse "true" $> JBool True
-         <|> stringParse "false" $> JBool False
+jBool :: Parser String JValue
+jBool = stringParse "true" $> JBool True
+     <|> stringParse "false" $> JBool False
 
-jsonCharParse :: Parser String Char
-jsonCharParse =   stringParse "\\\"" $> '"'
+jsonchar :: Parser String Char
+jsonchar =   stringParse "\\\"" $> '"'
         <|> stringParse "\\\\" $> '\\'
         <|> stringParse "\\/"  $> '/'
         <|> stringParse "\\b"  $> '\b'
@@ -197,17 +213,167 @@ combineSurrogates :: Char -> Char -> Char
 combineSurrogates a b = chr $
     ((ord a - 0xD800) `shiftL` 10) + (ord b - 0xDC00) + 0x10000
 
-jStringParse :: Parser String JValue
-jStringParse = JString <$> (charParse '"' *> jString') -- we test for the
+jString :: Parser String JValue
+jString = JString <$> (char '"' *> jString') -- we test for the
     where
         jString' = do
-            optFirst <- optional jsonCharParse -- we get the first character, using optional because it may not exist
+            optFirst <- optional jsonchar -- we get the first character, using optional because it may not exist
             case optFirst of
-                Nothing -> "" <$ charParse '"' -- if there is no first character the input is empty
+                Nothing -> "" <$ char '"' -- if there is no first character the input is empty
                 Just first | not $ isSurroagate first ->
                     (first:) <$> jString' -- if the first character is not a surrogate then we treat it as a normal character and call jString' recursively
                 Just first -> do
-                    second <- jsonCharParse -- otherwise we parse the second character
+                    second <- jsonchar -- otherwise we parse the second character
                     if isHighSurrogate first && isLowSurrogate second -- if there is a valid surrogate pair
                     then (combineSurrogates first second:) <$> jString' -- we combine the surrogates and continue reccursively
                     else empty -- otherwise we fail because the surrogate pair was invalid
+
+
+prop_genParseJString :: Property
+prop_genParseJString =
+    forAllShrink jStringGen shrink $ \js ->
+        case runParser jString (show js) of
+            Nothing     -> False
+            Just (_,o)  -> o == js
+
+
+-- JNumber parser
+
+jUInt :: Parser String Integer
+jUInt = (\d ds -> digitsToNumber 10 0 (d:ds) ) <$> digit19 <*> digits
+        <|> fromIntegral <$> digitParse
+
+-- digit parser excluding 0
+digit19 :: Parser String Int
+digit19 = digitToInt <$> satisfy (\x -> isDigit x && x /= '0')
+
+digits :: Parser String [Int]
+digits = some digitParse
+
+jInt :: Parser String Integer
+jInt = signInt <$> optional (char '-') <*> jUInt
+
+signInt :: Maybe Char -> Integer -> Integer
+signInt (Just '-') i = negate i
+signInt _          i = i
+
+jFrac :: Parser String [Int]
+jFrac = char '.' *> digits
+
+jExp :: Parser String Integer
+jExp = (char 'e' <|> char 'E')
+    *> (signInt <$> optional (char '+' <|> char '-') <*> jUInt)
+
+
+-- solution provided by the site
+
+-- jInt :: Parser String JValue
+-- jInt = JNumber <$> jInt' <*> pure [] <*> pure 0
+--
+-- jIntExp :: Parser String JValue
+-- jIntExp = JNumber <$> jInt' <*> pure [] <*> jExp
+--
+-- jIntFrac :: Parser String JValue
+-- jIntFrac = JNumber <$> jInt' <*> jFrac <*> pure 0
+--
+-- jIntFracExp :: Parser String JValue
+-- jIntFracExp = JNumber <$> jInt' <*> jFrac <*> jExp
+--
+-- jNumber :: Parser String JValue
+-- jNumber = jIntFracExp <|> jIntExp <|> jIntFrac <|> jInt
+
+-- a more clean solution written by me
+jNumber :: Parser String JValue
+jNumber = JNumber <$> jInt
+                  <*> (jFrac <|> pure [])
+                  <*> (jExp <|> pure 0)
+
+prop_genParseJNumber :: Property
+prop_genParseJNumber =
+    forAllShrink jNumberGen shrink $ \jn ->
+        case runParser jNumber (show jn) of
+            Nothing    -> False
+            Just (_,o) -> o == jn
+
+-- JArray Parser
+surroundedBy :: Parser String a -> Parser String b -> Parser String a
+surroundedBy p1 p2 = p2 *> p1 <* p2
+
+-- a way to see if there are separators between every value
+separatedBy :: Parser i v -> Parser i s -> Parser i [v]
+separatedBy v s = (:) <$> v <*> many (s *> v)
+               <|> pure []
+
+spaces :: Parser String String
+spaces = many (char ' ' <|> char '\n' <|> char '\r' <|> char '\t')
+
+jArray :: Parser String JValue
+jArray = JArray <$>
+    (char '[' *>
+        (jValue `separatedBy` char ',' `surroundedBy` spaces)
+    <* char ']')
+
+jObject :: Parser String JValue
+jObject = JObject <$>
+    (char '{' *> pair `separatedBy` char ',' `surroundedBy` spaces <* char '}')
+    where
+        pair = (\(JString k) v -> (k, v))
+           <$> (jString `surroundedBy` spaces)
+           <* char ':'
+           <*> jValue
+
+-- properties for jArray and jObject
+
+prop_genParseJArray :: Property
+prop_genParseJArray =
+    forAllShrink (sized jArrayGen) shrink $ \ja -> do
+        jas <- dropWhile isSpace <$> stringify ja
+        return . counterexample (show jas) $ case runParser jArray jas of
+            Nothing     -> False
+            Just (_, o) -> o == ja
+
+prop_genParseJObject :: Property
+prop_genParseJObject =
+    forAllShrink (sized jObjectGen) shrink $ \ja -> do
+        jas <- dropWhile isSpace <$> stringify ja
+        return . counterexample (show jas) $ case runParser jObject jas of
+            Nothing     -> False
+            Just (_, o) -> o == ja
+
+
+jValue :: Parser String JValue
+jValue = jValue' `surroundedBy` spaces
+  where
+    jValue' =  jNull
+           <|> jBool
+           <|> jString
+           <|> jNumber
+           <|> jArray
+           <|> jObject
+
+parseJSON :: String -> Maybe JValue
+parseJSON s = case runParser jValue s of
+    Just ("", json) -> Just json
+    _               -> Nothing
+
+prop_genParseJSON :: Property
+prop_genParseJSON = forAllShrink (sized jValueGen) shrink $ \value -> do
+  json <- stringify value
+  return . counterexample (show json) . (== Just value) . parseJSON $ json
+
+runTests :: IO ()
+runTests = do
+    putStrLn "~~ verifying JString ~~"
+    quickCheck prop_genParseJString
+
+    putStrLn "~~ verifying JNumber ~~"
+    quickCheck prop_genParseJNumber
+
+    putStrLn "~~ verifying JArray ~~"
+    quickCheck prop_genParseJArray
+
+    putStrLn "~~ verifying JObject ~~"
+    quickCheck prop_genParseJObject
+
+    putStrLn "~~ verifying JSON ~~"
+    quickCheck prop_genParseJSON
